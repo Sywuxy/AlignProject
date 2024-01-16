97ca8403c406e7f36943cf9299a0d6eac1b75a6c
Daniil Fadeev <daniilfadeev2018@gmail.com>
2024-01-16 07:39:36 +0200

    add initial logic and markUp for hidden items

--- js/index.js
+++ js/index.js
@@ -5,4 +5,19 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     body.classList.toggle('active');
     burgerNav.classList.toggle('active');
-})
+});
+
+function toggleImg() {
+    const pagesBtn = document.querySelector('.pages__button');
+    const companyItems = document.querySelectorAll('.company__item');
+
+    for(let i = 3; i < companyItems.length; i++) {
+        companyItems[i].classList.toggle('pages__show');
+    }
+
+    if (pagesBtn.innerHTML === 'View all'){
+        pagesBtn.innerHTML = 'Close';
+    } else {
+        pagesBtn.innerHTML = 'View all';
+    }
+}
