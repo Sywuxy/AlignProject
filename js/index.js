8208d8ef69ceec193af22ca032690da348f130c3
Daniil Fadeev <daniilfadeev2018@gmail.com>
2024-01-16 08:12:26 +0200

    add cms section

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
 const burger = document.querySelector('.burger');
 const body = document.querySelector('body');
-const burgerNav = document.querySelector('.burger__nav')
+const burgerNav = document.querySelector('.burger__nav');
+const upbtn = document.querySelector('.upBtn');
+
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     body.classList.toggle('active');
     burgerNav.classList.toggle('active');
+    upbtn.classList.toggle('active');
 });
 
 function toggleImg() {
-    const pagesBtn = document.querySelector('.pages__button');
+    const pagesBtn = document.querySelectorAll('.pages__button');
     const companyItems = document.querySelectorAll('.company__item');
 
     for(let i = 3; i < companyItems.length; i++) {
         companyItems[i].classList.toggle('pages__show');
     }
 
-    if (pagesBtn.innerHTML === 'View all'){
-        pagesBtn.innerHTML = 'Close';
-    } else {
-        pagesBtn.innerHTML = 'View all';
+    for (let i = 0; i < pagesBtn.length; i++) {
+        if (pagesBtn[i].innerHTML === 'View all'){
+            pagesBtn[i].innerHTML = 'Close';
+        } else {
+            pagesBtn[i].innerHTML = 'View all';
+        }
     }
 }
